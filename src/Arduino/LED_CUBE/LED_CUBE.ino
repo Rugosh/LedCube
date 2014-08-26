fdbed13d2b44e91ba236c38b0d470cfc5178b528
Rugosh <tobias.s.kuhn@gmail.com>
2014-08-26 21:46:31 +0200

    Basic Arduino Sketch setup and initialising the project

---
+++ src/Arduino/LED_CUBE/LED_CUBE.ino
@@ -0,0 +1,70 @@
+/*
+  Pin Settings
+*/
+const int DS_pin = 8;
+const int STCP_pin = 9;
+const int SHCP_pin = 10;
+
+const int MR_pin = 7;
+
+/*
+  Init the System
+*/
+void setup(){
+  // initialize serial:
+  Serial.begin(9600);
+  
+  pinMode(DS_pin, OUTPUT);
+  pinMode(STCP_pin, OUTPUT);
+  pinMode(SHCP_pin, OUTPUT);
+  pinMode(MR_pin, OUTPUT);
+
+  writeShift();
+}
+
+/*
+  Stores the Values of the registers
+*/
+boolean shiftRegValues[32];
+
+/*
+  Writes the content of the shift register value array out.
+*/
+void writeShift(){
+  Serial.println("-->writeShift");
+  Serial.print("-->");
+  
+  digitalWrite(SHCP_pin, LOW);
+  
+  for(int i = 31; i >= 0; i--){
+    Serial.print(shiftRegValues[i]);
+    
+    digitalWrite(STCP_pin, LOW);
+    digitalWrite(DS_pin, shiftRegValues[i]);
+    digitalWrite(STCP_pin, HIGH);
+  }
+  
+  Serial.println("");
+  
+  digitalWrite(SHCP_pin, HIGH);
+}
+
+/*
+  Main LOOP
+*/
+void loop(){
+  // Reset the shift registers
+  digitalWrite(MR_pin, LOW);
+  digitalWrite(MR_pin, HIGH);
+  
+  // DO some magic
+  
+  writeShift();
+  delay(500);
+
+}
+
+
+
+
+
